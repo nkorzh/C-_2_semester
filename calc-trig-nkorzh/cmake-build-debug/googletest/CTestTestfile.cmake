e8b18336bf6fb7216900b032120a191add791b65
Nikita Kozhukharov <n.korzhikov@gmail.com>
2020-09-28 20:08:03 +0300

    deleted trash

--- calc-trig-nkorzh/cmake-build-debug/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/DOC/Documents/Progs/C++/C++ 2 sem/calc-trig-nkorzh/googletest
-# Build directory: D:/DOC/Documents/Progs/C++/C++ 2 sem/calc-trig-nkorzh/cmake-build-debug/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- calc-trig-nkorzh/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/DOC/Documents/Progs/C++/C++ 2 sem/calc-trig-nkorzh/googletest/googlemock
-# Build directory: D:/DOC/Documents/Progs/C++/C++ 2 sem/calc-trig-nkorzh/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
